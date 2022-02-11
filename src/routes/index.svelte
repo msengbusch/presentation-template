fc6bf0fc00405aa1d4f9a12298b451439797a99a
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-02-11 16:15:19 +0100

    basic presentation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,15 +3,40 @@
 </script>
 
 <script lang="ts">
+  import 'reveal.js/dist/reveal.css';
+  import 'reveal.js/dist/theme/black.css';
+
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const Reveal = (await import('reveal.js')).default;
+
+    await Reveal.initialize();
+  });
 </script>
 
 <svelte:head>
   <title>Home</title>
 </svelte:head>
 
-<section>
-  <h1>Test</h1>
-</section>
+<div class="reveal">
+  <div class="slides">
+    <section>
+      <h2>Barebones Presentation</h2>
+      <p>
+        This example contains the bare minimum includes and markup required to run a reveal.js
+        presentation.
+      </p>
+    </section>
+    <section>
+      <h2>No Theme</h2>
+      <p>There's no theme included, so it will fall back on browser defaults.</p>
+    </section>
+  </div>
+</div>
 
 <style>
+  .reveal {
+    position: absolute;
+  }
 </style>
